1ae74bed005d6d67ae40f31895b3590ddd53c5a8
35018 <nhs35018@nhs.hal.ac.jp>
2025-05-19 09:34:39 +0900

    googleフォント導入確認

--- front/front/src/app/news/page.tsx
+++ front/front/src/app/news/page.tsx
@@ -5,7 +5,7 @@ import "./news.css"
 const News = ()=>{
     return(
         <div id={"wrapNews"}>
-            <h2 id={"newsTitle"}>NEWS</h2>
+            <h2 id={"newsTitle"}>Information</h2>
 
             <div className={"articles"}>
                 <div className={"art"}>
