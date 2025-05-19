466d1e1a3687c40163eb3786fcf614be4dd235ff
35018 <nhs35018@nhs.hal.ac.jp>
2025-05-19 11:41:10 +0900

    componentディレクトリ追加

---
+++ front/front/src/app/access/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+import './access.css'
+import Header from "@/app/components/header/page";
+
+const Access = () =>{
+    return(
+        <Header></Header>
+    );
+}
+
+export default Access;
---
+++ front/front/src/app/movies/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+import './movies.css'
+import Header from "@/app/components/header/page";
+
+const Movies = () =>{
+    return(
+        <Header></Header>
+    );
+}
+
+export default Movies;
