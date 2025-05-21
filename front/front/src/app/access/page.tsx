61ce0799b3d147c82a66a25578bffcdfe5e95308
35018 <nhs35018@nhs.hal.ac.jp>
2025-05-21 15:38:02 +0900

    accessページ大枠

--- front/front/src/app/access/page.tsx
+++ front/front/src/app/access/page.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import './access.css'
 import Header from "@/app/components/header/page";
+import Image from "next/image";
+import Screen01 from "@/public/img/Screen01.png"
 
 const Access = () =>{
     return(
@@ -12,7 +14,7 @@ const Access = () =>{
                     <h1>Access</h1>
                     <p>施設案内</p>
                 </div>
-                <div className={"acInfo"} id={"access"}>
+                <div className={"acInfo"} id={"acAccess"}>
                     <h2 className={"infoTitle"}>アクセス</h2>
                     <div className={"acContent"}>
                         <iframe id={"map"}
@@ -20,23 +22,48 @@ const Access = () =>{
                                 width="500" height="400" loading="lazy"
                                 referrerPolicy="no-referrer-when-downgrade"></iframe>
                         <p className={"ml-10 mt-2"}>
-                            〒450-0002 愛知県名古屋市中村区名駅４丁目２７−１<br/> 総合校舎スパイラルタワーズ<br/>
+                            〒450-0002 愛知県名古屋市中村区名駅４丁目２７−１<br/> 総合校舎スパイラルタワーズ<br/><br/>
                             名駅から徒歩5分<br/>
                             モード学園スパイラルタワーズ内
                         </p>
                     </div>
-
-
                 </div>
-                <div className={"acInfo"} id={"equip"}>
+
+                <div className={"acInfo"} id={"acEquip"}>
                     <h2 className={"infoTitle"}>劇場設備</h2>
+
+                    <div className={'acContent flex-row justify-around'} id={"equipList"}>
+                        <div className={'equip'}>
+                            <Image src={Screen01} alt={"Screen01"} width={300} height={200}></Image>
+                            <h3>大スクリーン 200席</h3>
+                            <p>スクリーン1</p>
+                            <p>スクリーン2</p>
+                            <p>スクリーン3</p>
+                        </div>
+                        <div className={'equip'}>
+                            <Image src={Screen01} alt={"Screen01"} width={300} height={200}></Image>
+                            <h3>中スクリーン 120席</h3>
+                            <p>スクリーン4</p>
+                            <p>スクリーン5</p>
+                        </div>
+                        <div className={'equip'}>
+                            <Image src={Screen01} alt={"Screen01"} width={300} height={200}></Image>
+                            <h3>小スクリーン 70席</h3>
+                            <p>スクリーン6</p>
+                            <p>スクリーン7</p>
+                            <p>スクリーン8</p>
+                        </div>
+                    </div>
+
                 </div>
-                <div className={"acInfo"} id={"fee"}>
+
+                <div className={"acInfo"} id={"acFee"}>
                     <h2 className={"infoTitle"}>チケット料金</h2>
-                    <table id={"feeList"}>
+
+                    <table className={"acContent"} id={"feeList"}>
                         <tbody>
                         <tr>
-                            <th>一般</th>
+                        <th>一般</th>
                             <th>大学生等</th>
                             <th>中学・高校</th>
                             <th>小学生以下</th>
@@ -47,12 +74,11 @@ const Access = () =>{
                             <td>1,400円</td>
                             <td>1,000円</td>
                         </tr>
-                        </tbody>
-
-                    </table>
+                    </tbody>
+                </table>
                 </div>
-
             </div>
+
         </>
     );
 }
