de3dc80d4e634f3fa9dd667fc877d0c570231659
sakakibar <senao11032@gmail.com>
2025-05-26 10:44:03 +0900

    movies変更

--- front/front/src/app/movies/page.tsx
+++ front/front/src/app/movies/page.tsx
@@ -1,11 +1,85 @@
 'use client';
 import React from 'react';
 import './movies.css'
-import Header from "@/app/components/header/page";
+import Link from 'next/link';
+//import Header from "@/app/components/header/page";
 
 const Movies = () =>{
-    return(
-        <Header></Header>
+        return(
+        <>
+            {/*<Header></Header>*/}
+        <div id={"Movie"}>
+            <h2 id={"MovieTitle"}>Movies</h2>
+
+            <div className={"articles"}>
+                <div className={"art"}>
+            <div className={"artImg mb-3"}>
+                <img src="/images/haru.jpg" className="w-full h-full object-cover" />
+            </div>
+            <p className={"artTitle"}>
+                <Link href="/movies/1">タイトル</Link>
+            </p>
+            </div>
+                <div className={"art"}>
+            <div className={"artImg mb-3"}>
+                <img src="/images/5cm.jpg" className="w-full h-full object-cover" />
+            </div>
+            <p className={"artTitle"}>
+                <Link href="/movies/1">タイトル</Link>
+            </p>
+            </div>
+                <div className={"art"}>
+            <div className={"artImg mb-3"}>
+                <img src="/images/same.jpg" className="w-full h-full object-cover" />
+            </div>
+            <p className={"artTitle"}>
+                <Link href="/movies/1">タイトル</Link>
+            </p>
+            </div>
+                <div className={"art"}>
+            <div className={"artImg mb-3"}>
+                <img src="/images/summer.jpg" className="w-full h-full object-cover" />
+            </div>
+            <p className={"artTitle"}>
+                <Link href="/movies/1">タイトル</Link>
+            </p>
+            </div>
+                <div className={"art"}>
+            <div className={"artImg mb-3"}>
+                <img src="/images/bee.jpg" className="w-full h-full object-cover" />
+            </div>
+            <p className={"artTitle"}>
+                <Link href="/movies/1">タイトル</Link>
+            </p>
+            </div>
+                <div className={"art"}>
+            <div className={"artImg mb-3"}>
+                <img src="/images/konan.jpg" className="w-full h-full object-cover" />
+            </div>
+            <p className={"artTitle"}>
+                <Link href="/movies/1">タイトル</Link>
+            </p>
+            </div>
+                <div className={"art"}>
+            <div className={"artImg mb-3"}>
+                <img src="/images/interstellar.jpg" className="w-full h-full object-cover" />
+            </div>
+            <p className={"artTitle"}>
+                <Link href="/movies/1">タイトル</Link>
+            </p>
+            </div>
+                <div className={"art"}>
+            <div className={"artImg mb-3"}>
+                <img src="/images/konan2.jpg" className="w-full h-full object-cover" />
+            </div>
+            <p className={"artTitle"}>
+                <Link href="/movies/1">タイトル</Link>
+            </p>
+            </div>
+
+            </div>
+        </div>
+        </>
     );
 }
 
