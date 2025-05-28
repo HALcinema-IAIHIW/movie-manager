7dda83482846281fd636f03c24fffb82dec34f49
sakakibar <senao11032@gmail.com>
2025-05-28 21:55:51 +0900

    movie

--- front/front/src/app/movies/page.tsx
+++ front/front/src/app/movies/page.tsx
@@ -13,68 +13,78 @@ const Movies = () =>{
 
             <div className={"articles"}>
                 <div className={"art"}>
-            <div className={"artImg mb-3"}>
+            <Link href="/movies/haru" className="block">
+                <div className={"artImg mb-3"}>
                 <img src="/images/haru.jpg" className="w-full h-full object-cover" />
+                </div>
+                <p className={"artTitle"}>四月は君の嘘</p>
+            </Link>
             </div>
-            <p className={"artTitle"}>
-                <Link href="/movies/1">タイトル</Link>
-            </p>
-            </div>
+
+
                 <div className={"art"}>
-            <div className={"artImg mb-3"}>
+            <Link href="/movies/byosoku" className='block'>
+                <div className={"artImg mb-3"}>
                 <img src="/images/5cm.jpg" className="w-full h-full object-cover" />
             </div>
-            <p className={"artTitle"}>
-                <Link href="/movies/1">タイトル</Link>
-            </p>
+            <p className={"artTitle"}>秒速5センチメートル</p>
+            </Link>
             </div>
+            
+            
                 <div className={"art"}>
+            <Link href="/movies/same" className='block'>
             <div className={"artImg mb-3"}>
                 <img src="/images/same.jpg" className="w-full h-full object-cover" />
             </div>
-            <p className={"artTitle"}>
-                <Link href="/movies/1">タイトル</Link>
+            <p className={"artTitle"}>MANEATER
             </p>
+            </Link>
             </div>
+            
                 <div className={"art"}>
+             <Link href="/movies/summer" className='block'>
             <div className={"artImg mb-3"}>
                 <img src="/images/summer.jpg" className="w-full h-full object-cover" />
             </div>
-            <p className={"artTitle"}>
-                <Link href="/movies/1">タイトル</Link>
-            </p>
+            <p className={"artTitle"}>サマーウォーズ</p>
+            </Link>
             </div>
+            
                 <div className={"art"}>
+            <Link href="/movies/bee" className='block'>
             <div className={"artImg mb-3"}>
                 <img src="/images/bee.jpg" className="w-full h-full object-cover" />
             </div>
-            <p className={"artTitle"}>
-                <Link href="/movies/1">タイトル</Link>
-            </p>
+            <p className={"artTitle"}>BEETLW JUICE</p>
+            </Link>
             </div>
+            
                 <div className={"art"}>
+            <Link href="/movies/konan" className='block'>
             <div className={"artImg mb-3"}>
                 <img src="/images/konan.jpg" className="w-full h-full object-cover" />
             </div>
-            <p className={"artTitle"}>
-                <Link href="/movies/1">タイトル</Link>
-            </p>
+            <p className={"artTitle"}>名探偵コナン</p>
+            </Link>
             </div>
+            
                 <div className={"art"}>
+            <Link href="/movies/interstellar" className='block'>
             <div className={"artImg mb-3"}>
                 <img src="/images/interstellar.jpg" className="w-full h-full object-cover" />
             </div>
-            <p className={"artTitle"}>
-                <Link href="/movies/1">タイトル</Link>
-            </p>
+            <p className={"artTitle"}>インターステラー</p>
+            </Link>
             </div>
+            
                 <div className={"art"}>
+            <Link href="/movies/konan2" className='block'>
             <div className={"artImg mb-3"}>
                 <img src="/images/konan2.jpg" className="w-full h-full object-cover" />
             </div>
-            <p className={"artTitle"}>
-                <Link href="/movies/1">タイトル</Link>
-            </p>
+            <p className={"artTitle"}>名探偵コナン</p>
+            </Link>
             </div>
 
             </div>
