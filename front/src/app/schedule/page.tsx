9e826db3d39f1218bd9913fda58acca5c031a62c
35018 <nhs35018@nhs.hal.ac.jp>
2025-06-04 17:16:20 +0900

    scheduleページ基本の見た目

---
+++ front/src/app/schedule/page.tsx
@@ -0,0 +1,85 @@
+'use client';
+import React from "react";
+import "./schedule.css"
+
+const MovieSchedule = [
+    {
+        id:1,
+        date:"0615",
+        moive:"Movie01",
+    },{
+        id:2,
+        date:"0615",
+        moive:"Movie02",
+    },{
+        id:3,
+        date:"0615",
+        moive:"Movie03",
+    },{
+        id:4,
+        date:"0616",
+        moive:"Movie01",
+    },{
+        id:5,
+        date:"0616",
+        moive:"Movie02",
+    },{
+        id:6,
+        date:"0616",
+        moive:"Movie03",
+    }
+]
+
+
+const Schedule = () =>{
+    return(
+        <div id={"scheduleBody"}>
+            <h1 id={"scheduleTitle"} className={"mb-5"}>上映スケジュール</h1>
+            <div id={"dateSlider"}>
+                <button className={"dateSlide"} id={"left"}>&lt;</button>
+                <div id={"dateList"}>
+                    <button className={"scDate"}>
+                        6/15
+                    </button>
+                    <button className={"scDate"}>
+                        6/16
+                    </button>
+                    <button className={"scDate"}>
+                        6/17
+                    </button>
+                    <button className={"scDate"}>
+                        6/18
+                    </button>
+                    <button className={"scDate"} >
+                        6/19
+                    </button>
+                    <button className={"scDate"} >
+                        6/20
+                    </button>
+                </div>
+
+                <button className={"dateSlide"} id={"right"}>&gt;</button>
+            </div>
+
+            <div id={"timeList"}>
+                <div className={"TL"}>
+                    <div className={"CinemaTL"}>
+                        <h2>Movie01</h2>
+                        <div className={"Movie-TL"}>
+                            <div className={"Poster bg-gray-500"}>ここにポスター</div>
+                            <button className={"Time"}>
+                                スクリーン1<br/>
+                                <span>9:30</span><br/>
+                                空席◎
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Schedule;
