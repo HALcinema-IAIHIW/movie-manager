8010334f080ffd35f3b3af8d1f48e08771dd5d61
35018 <nhs35018@nhs.hal.ac.jp>
2025-06-09 12:25:35 +0900

    Swiper導入

--- front/src/app/schedule/page.tsx
+++ front/src/app/schedule/page.tsx
@@ -1,7 +1,39 @@
 'use client';
 import React from "react";
 import "./schedule.css"
+// Swiper導入
+import {Swiper,SwiperSlide} from "swiper/react";
+import {Navigation} from "swiper/modules";
+import "swiper/css"
+import "swiper/css/navigation"
+
+// コンポーネント
 import MovieTL from "@/app/components/MovieTL/page";
+
+const ViewDate = [
+    {
+        id:1,
+        date:"0615"
+    },{
+        id:2,
+        date:"0616"
+    },{
+        id:3,
+        date:"0617"
+    },{
+        id:4,
+        date:"0618"
+    },{
+        id:5,
+        date:"0619"
+    },{
+        id:6,
+        date:"0620"
+    },{
+        id:7,
+        date:"0621"
+    },
+]
 
 const MovieSchedule = [
     {
@@ -37,31 +69,27 @@ const Schedule = () =>{
     return(
         <div id={"scheduleBody"}>
             <h1 id={"scheduleTitle"} className={"mb-5"}>上映スケジュール</h1>
+            {/*　日付　*/}
             <div id={"dateSlider"}>
-                <button className={"dateSlide"} id={"left"}>&lt;</button>
-                <div id={"dateList"}>
-                    <button className={"scDate"}>
-                        6/15
-                    </button>
-                    <button className={"scDate"}>
-                        6/16
-                    </button>
-                    <button className={"scDate"}>
-                        6/17
-                    </button>
-                    <button className={"scDate"}>
-                        6/18
-                    </button>
-                    <button className={"scDate"} >
-                        6/19
-                    </button>
-                    <button className={"scDate"} >
-                        6/20
-                    </button>
-                </div>
+                <Swiper
+                    modules={[Navigation]}
+                    navigation={true}
+                    spaceBetween={50}
+                    slidesPerView={3}
+                    >
+                    {ViewDate.map((day)=>(
+                        <SwiperSlide key={day.id}>
 
-                <button className={"dateSlide"} id={"right"}>&gt;</button>
+                            <button className={"scDate"} >
+                                    {day.date}
+                            </button>
+                    </SwiperSlide>
+                ))
+                }
+
+            </Swiper>
             </div>
+
             <hr/>
             <div id={"timeList"}>
                 <div className={"CinemaTL"}>
