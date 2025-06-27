8fe20ecdccd3700418d04afd66beed8b9dee8915
hqrna <horibe.h0113@gmail.com>
2025-06-28 01:04:13 +0900

    screen-apiの修正

--- back/src/datastructure/request/screen.go
+++ back/src/datastructure/request/screen.go
@@ -1,6 +1,6 @@
 package request
 
 type CreateScreenRequest struct {
-	MaxRow    string `json:"max_row" binding:"required,max=10"`   // ✅ 文字列の長さ制限
-	MaxColumn int    `json:"max_column" binding:"required,gte=1"` // ✅ 数値の範囲制限
+	MaxRow    string `json:"max_row" binding:"required,max=10"`
+	MaxColumn int    `json:"max_column" binding:"required,gte=1"`
 }
