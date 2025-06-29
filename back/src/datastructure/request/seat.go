b07e047a55ef54996222ecc1e60ccefa06d954cc
sano40224 <yuuki130908@gmail.com>
2025-06-29 18:36:58 +0900

    Update seat.go

--- back/src/datastructure/request/seat.go
+++ back/src/datastructure/request/seat.go
@@ -1,6 +1,5 @@
 package request
 
-// CreateSeatRequest は座席作成のリクエストボディを表します
 type CreateSeatRequest struct {
 	ScreenID   uint   `json:"screen_id" binding:"required"`
 	Row        string `json:"row" binding:"required"`
