47a6e7dfbd9b28b67a5b1b6f69533c69020ba418
35018 <nhs35018@nhs.hal.ac.jp>
2026-02-18 18:43:27 +0900

    完了画面ボタン配置しました　以後よろしく (#200)

--- front/src/app/admin/tickets/completed/page.tsx
+++ front/src/app/admin/tickets/completed/page.tsx
@@ -45,25 +45,8 @@ function CompletedContent() {
 
     return(
         <div id={"completedMain"} className={"flex flex-col mt-32 items-center justify-center"}>
-            <h1>ここはAdminの購入完了画面です</h1>
-            <h1 className={"text-4xl mb-6 font-semibold tracking-wide"}>購入手続きが完了しました</h1>
-            <h2 className={"text-2xl mb-16"}>ご購入ありがとうございました</h2>
+            <h1 className={"text-4xl mb-6 font-semibold tracking-wide"}>購入完了</h1>
 
-            <div id={"toEnter"} className={"bg-dark-lighter w-3/4 p-5 mb-8"}>
-                <h2 className={"text-2xl mb-5 text-gold font-semibold tracking-wide"}>入場方法</h2>
-                <div id={"admission-number"} className={"mb-5 w-3/4 flex justify-between mx-auto"}>
-                    <h2 className={"text-xl mb-2"}>チケットを発券して入場</h2>
-                    <p className={" block bg-gray-700 px-6 py-3 tracking-widest text-lg  text-center text-gold"}>12345</p>
-                </div>
-
-                <div id={"admission-qr"} className={"mb-3 w-3/4 flex justify-between mx-auto"}>
-                    <h2 className={"text-xl mb-2"}>QRコードで入場</h2>
-                    <button className="btn-outline-luxury flex items-center justify-center gap-2 font-jp">
-                        <QrCode size={18}/>
-                        QRコード
-                    </button>
-                </div>
-            </div>
 
             <div id={"showDetail"} className={"bg-dark-lighter w-3/4 p-5 mb-5"}>
                 <h3 className={"text-2xl mb-5 text-gold font-semibold tracking-wide"}>購入情報</h3>
@@ -115,9 +98,12 @@ function CompletedContent() {
                     <span className={"text-xl mx-2.5"}>{Number(totalPrice).toLocaleString()}</span> 円
                 </p>
             </div>
-            <p className={"mb-5"}>マイページでもご確認いただけます</p>
-            <Link className={"bg-gold py-3 px-4 rounded-lg text-darkest hover:scale-90 transition-all "}
-                  href={"../mypage"}>マイページ</Link>
+            <div id={"pdf"}　className={"flex flex-row gap-20 mb-10"}>
+                <button type={"button"} className={"py-3 px-4 rounded-lg border-2 border-dark-lighter bg-gold text-dark hover:shadow-accent"}>チケット発券</button>
+                <button type={"button"} className={"py-3 px-4 rounded-lg border-2 border-dark-lighter bg-gold text-dark hover:shadow-white"}>領収書発行</button>
+            </div>
+            <Link className={"bg-dark-lighter py-3 px-4 rounded-lg text-gold border-2 border-gold hover:scale-90 transition-all "}
+                  href={"../tickets/schedule"}>上映回選択に戻る</Link>
         </div>
     )
 }
